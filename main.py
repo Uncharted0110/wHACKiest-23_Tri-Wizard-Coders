c9e505cf98c1c9066f318c042cfaa251c1e16209
a-r-surpurm <115961558+a-r-surpurm@users.noreply.github.com>
2023-01-27 19:52:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import speech_recognition as sr
 import pyaudio
 import pocketsphinx
+import pyttsx3
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
+    command=r.recognize_google(audio)
     try:
         #print(type(r))
-        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+        print(f"You said \"{command}\" " )
     except sr.UnknownValueError:
         print("Sphinx could not understand audio")

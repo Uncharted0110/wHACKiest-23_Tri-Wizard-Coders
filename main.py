bc3fe9774146e1e9ec9a000455ecbc0475721f5a
Chirantan28 <120579701+Chirantan28@users.noreply.github.com>
2023-01-27 20:47:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import speech_recognition as sr
-import pyaudio
-import pocketsphinx
-import pyttsx3
-r = sr.Recognizer()
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-    command=r.recognize_google(audio)
-    try:
-        #print(type(r))
-        print(f"You said \"{command}\" " )
-    except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
+def print_to_pdf():
+    # Create a file named 'output.pdf' in the current directory
+    with open('output.pdf', 'wb') as pdf_file:
+        # Create a canvas object, using the letter page size
+        c = canvas.Canvas(pdf_file, pagesize=letter)
+        
+        # Write text to the canvas
+        c.drawString(100, 750, "Hello, World!")
+        
+        # Save the canvas
+        c.save()
+
+print_to_pdf()
